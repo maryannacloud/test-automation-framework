ac6c157f51a9f8b4144615723140df8a72c6e30e
marynanesterenko <marynanesterenko.qa@gmail.com>
2025-01-26 22:53:36 -0500

    Securing config data

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
+  env: {
+    appUrl: process.env.APP_URL,
+    userEmail: process.env.USER_EMAIL,
+  },
+  defaultCommandTimeout: 10000,
   e2e: {
-    baseUrl: 'https://cleerlyhealth.com',
-    supportFile: 'cypress/support/e2e.ts'  
+    baseUrl: process.env.APP_URL,
+    setupNodeEvents(on, config) {
+      
     },
-  });
+  },
+});
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,32 +1,32 @@
 declare namespace Cypress {
-    interface Chainable {
+  interface Chainable {
       /**
        * Custom command to log in via email.
        * @param email - email address to use for login
        */
       loginWithEmail(email: string): Chainable<void>;
-    }
+
+      /**
+       * Custom command to restore a session using a JWT token stored in the Cypress environment.
+       */
+      restoreSessionWithJWT(): Chainable<void>;
+
+      /**
+       * Custom command to log in via API and store the JWT token in Cypress environment.
+       * @param email - The email address to use for login
+       */
+      loginWithJWT(email: string): Chainable<void>;
+
+      /**
+       * Custom command to check a header's text.
+       * @param headerText - The text to check in the header
+       */
+      checkHeader(headerText: string): Chainable<Element>;
+
+      /**
+       * Custom command to assert that an element is visible.
+       * @param selector - The CSS selector of the element
+       */
+      assertElementVisible(selector: string): Chainable<Element>;
   }
-
-declare namespace Cypress {
-    interface Chainable {
-        checkHeader(headerText: string): Chainable<Element>;
-        assertElementVisible(selector: string): Chainable<Element>;
-    }
-  }
-
-declare namespace Cypress {
-    interface Chainable {
-        /**
-         * Custom command to log in via API and store the JWT token in Cypress environment.
-         * @param email - The email address to use for login
-         * @param password - The password to use for login
-         */
-        loginWithJWT(email: string, password: string): Chainable<void>;
-
-        /**
-         * Custom command to restore a session using a JWT token stored in the Cypress environment.
-         */
-        restoreSessionWithJWT(): Chainable<void>;
-}
 }
